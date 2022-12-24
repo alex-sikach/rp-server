fb65aef6421f4dd8c5037063b56f762c66585309
alex-sikach <hlophlop1245@gmail.com>
2022-12-24 14:13:37 +0200

    initial javascript

---
+++ public/index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+const app = express();
+const port = process.env.PORT || 5000;
+//middleware
+app.use(cors());
+app.use(express.json());
+// ROUTES
+app.listen(port, async () => {
+    try {
+        console.log(`server has started on port ${port}`);
+        // todo: deleting all db sessions
+        // todo: restarting users_is_seq sequence
+    }
+    catch (e) {
+        console.log(e);
+    }
+});
